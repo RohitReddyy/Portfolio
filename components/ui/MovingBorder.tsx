8cefcbf863493fa21166d18b266b1042a315f3d1
rmusukudabbidi <rmusukudabbidi@slu.edu>
2024-11-06 12:24:31 -0600

    Resolving errors

--- components/ui/MovingBorder.tsx
+++ components/ui/MovingBorder.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 "use client";
 import React, { useRef } from "react";
 import {
@@ -134,3 +135,5 @@ export const MovingBorder = ({
     </>
   );
 };
+
+/* eslint-enable @typescript-eslint/no-explicit-any */
